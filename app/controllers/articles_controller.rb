f22481bca4e284235d6a670457b183f26b15f769
Vladimir <v.puhov7@gmail.com>
2019-06-26 23:49:04 +0300

    save article in db, output list of articles in index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,28 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @list_of_articles = Article.all.order(created_at: :desc)
+  end
+
   def new 
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new article_params
+
+    if @article.valid?
+      @article.save
+      render action: 'index'
+    else
+      render action: 'new'
+    end
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end  
 
 end
