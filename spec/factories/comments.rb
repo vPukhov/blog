b1459317ba74d3ce08afd0b9f499f24b4f3f2819
Vladimir <v.puhov7@gmail.com>
2019-07-19 22:45:18 +0300

    Add create contact acceptance test

---
+++ spec/factories/comments.rb
@@ -0,0 +1,7 @@
+
+FactoryBot.define do
+  factory :comment do
+    author { "Mike" }
+    sequence(:body) { |n| "comment body #{n}" }
+  end
+end
---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe "something" do
+  it "does something" do
+    # ...
+  end
+end
