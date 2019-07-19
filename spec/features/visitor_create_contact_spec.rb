b52d179da97d4200e2913d300a5b9d4d75e4faa6
Vladimir <v.puhov7@gmail.com>
2019-07-19 23:15:34 +0300

    Update "Contact us" in i18n format

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,9 +4,11 @@ feature "Contact creation" do
   
   # проверяем доступность страницы '/contacts' 
   scenario "allows acees to contacts page" do
+    
     # заходим на страницу
     visit '/contacts'
 
     expect(page).to have_content 'Contact us'    
-  end  
+  end
+
 end
