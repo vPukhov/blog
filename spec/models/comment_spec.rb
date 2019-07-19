10ec9754b2fafb97a11841f71ea431ff32522bd7
Vladimir <v.puhov7@gmail.com>
2019-07-19 22:07:42 +0300

    Add test for max length of body in Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Comment do
   it { should belong_to :article }
+
+  describe "Validate of length" do
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
+
 end
